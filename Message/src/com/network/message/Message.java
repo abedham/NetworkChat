63bbf04ace52ce397b02993a6753864957d0c829
Abdelrahman <abed.ha15@gmail.com>
2017-04-11 23:58:11 +0300

    Add Message project

---
+++ Message/src/com/network/message/Message.java
@@ -0,0 +1,61 @@
+package com.network.message;
+
+import java.io.Serializable;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Abed
+ */
+public class Message implements Serializable {
+
+    private String userName;
+    private String reciverName;
+    private String fileName;
+    private byte type;
+    private Object data;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getReciverName() {
+        return reciverName;
+    }
+
+    public void setReciverName(String reciverName) {
+        this.reciverName = reciverName;
+    }
+
+    public byte getType() {
+        return type;
+    }
+
+    public void setType(byte type) {
+        this.type = type;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
